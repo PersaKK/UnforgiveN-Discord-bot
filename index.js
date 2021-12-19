15e612e16e1907d7097fa2d0835a1c0cf2952144
PersaK <62612444+PersaKK@users.noreply.github.com>
2021-12-19 14:46:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const moment =require("moment");
 require("moment-duration-format");
                          
 const client = new Advanced.Client({
-    token: "tokeniniz buraya yazılmalı", 
+    token: "-----", 
     prefix: "!", 
     sahip: ["396352062008459266"], 
     komutDosya: "komutlar",
